4a8be47cbb5b389a53e9a49edfba8c6a41f81b0e
Xiao G. Wu <xiaogwu@gmail.com>
2013-08-26 19:09:47 -0700

    Add omniauth-twitter
    * Added home controller and view for landing page
    * Added sessions controller to create and destroy sessions
    * Added user model to find user by provider and uid or create new user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def create
+    # raise env['omniauth.auth'].to_yaml
+    user = User.from_omniauth(env['omniauth.auth'])
+    session[:user_id] = user.id
+    redirect_to root_url, notice: "Signed in."
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Signed out."
+  end
+
+  def failure
+    redirect_to root_url, alert: "Authentication failed, please try again."
+  end
+end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer unless Rails.env.production?
   provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']

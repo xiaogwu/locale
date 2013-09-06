e9d4588de835ee0d054c2956833f918404db40cb
Xiao G. Wu <xiaogwu@gmail.com>
2013-09-06 13:01:14 -0700

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.delete_all
+
+p1 = Post.create(:body => 'Hey this is a fake, seeded, post from the Richmond', :lat => 37.775, :lng => -122.486)
+p2 = Post.create(:body => 'This is another fake, seeded, post from the Richmond', :lat => 37.775, :lng => -122.486)
+p3 = Post.create(:body => 'This is fake, seeded, post from Diamond Heights', :lat => 37.737, :lng => -122.443)
